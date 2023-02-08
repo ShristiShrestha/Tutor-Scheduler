9ce8a2af833fad805c8ec95b163cf75d6376b8a3
Sumit Dhungana <sumitdhungana14@gmail.com>
2023-02-08 11:01:39 -0600

    Initialized a new spring boot project.

---
+++ src/main/java/com/fours/onlineschedulerapi/OnlineSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.fours.onlineschedulerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fours/onlineschedulerapi/OnlineSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fours.onlineschedulerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
