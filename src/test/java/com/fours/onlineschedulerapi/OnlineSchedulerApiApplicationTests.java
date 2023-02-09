ea33430564eb4604d98aebed4751a076d6719b16
Sumit Dhungana <sumitdhungana14@gmail.com>
2023-02-09 12:28:26 -0600

    Dockerize online-scheduler-api application.

--- src/test/java/com/fours/onlineschedulerapi/OnlineSchedulerApiApplicationTests.java
+++ src/test/java/com/fours/onlineschedulerapi/OnlineSchedulerApiApplicationTests.java
@@ -1,12 +1,10 @@
 package com.fours.onlineschedulerapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineSchedulerApiApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
