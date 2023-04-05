e46f45893f03ae369ecf0b63ccbbef2a4270d625
Shristi Shrestha <sthashristi777@gmail.com>
2023-04-05 02:40:02 -0500

    disable cors in websecurity

--- server/src/main/java/com/fours/onlineschedulerapi/configs/WebSecurityConfig.java
+++ server/src/main/java/com/fours/onlineschedulerapi/configs/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.cors().disable().csrf().disable()
             .authorizeRequests()
             .antMatchers("/actuator").permitAll()
             .antMatchers("/actuator/*").permitAll()
