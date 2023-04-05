5e80fb71a7bc011370d42469513efd2c187ed074
Shristi Shrestha <sthashristi777@gmail.com>
2023-04-05 02:59:00 -0500

    adds cors header, head method

--- server/src/main/java/com/fours/onlineschedulerapi/configs/CorsConfig.java
+++ server/src/main/java/com/fours/onlineschedulerapi/configs/CorsConfig.java
@@ -14,11 +14,17 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("Content-Type", "Authorization")
-                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "Access-Control-Max-Age", "Access-Control-Expose-Headers", "Authorization")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+                                "Access-Control-Allow-Methods"," Access-Control-Request-Headers",
+                                "Access-Control-Max-Age", "Access-Control-Expose-Headers", "Authorization")
                         .allowCredentials(false).maxAge(3600);
             }
         };
+
+//        registry.addMapping("/**")
+//                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+//                .allowedOrigins("http://localhost:3000")
     }
 }
