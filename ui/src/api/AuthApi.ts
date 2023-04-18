dd2ec10f628dce2a89cf7217162b952e411737b5
ShristiShrestha <sthashristi777@gmail.com>
2023-04-18 13:00:31 -0500

    fix signup issue reqd iscoordinator param

--- ui/src/api/AuthApi.ts
+++ ui/src/api/AuthApi.ts
@@ -1,7 +1,7 @@
 import Api from "../utils/ApiUtils";
-import {UserMiniDetailsType} from "../redux/user/types";
-import {SignupType} from "../redux/auth/types";
-import {capitalize} from "../utils/StringUtils";
+import { UserMiniDetailsType } from "../redux/user/types";
+import { SignupType } from "../redux/auth/types";
+import { capitalize } from "../utils/StringUtils";
 
 export const authenticate = () => {
     return Api.apiCall<UserMiniDetailsType>({
@@ -11,23 +11,26 @@ export const authenticate = () => {
 };
 
 // todo: /user/signup
-export const signup = (data) => {
+export const signup = data => {
     const signupData: SignupType = {
         ...data,
-        name: capitalize(data["name"] || data["email"]?.split("@")[0] || "no name"),
+        name: capitalize(
+            data["name"] || data["email"]?.split("@")[0] || "no name",
+        ),
         isTutor: false,
-    }
+        isCoordinator: false,
+    };
     return Api.apiCall<UserMiniDetailsType>({
         url: "/user/signup",
         method: "POST",
-        data: signupData
-    })
-}
+        data: signupData,
+    });
+};
 
-export const login = (data) => {
+export const login = data => {
     return Api.apiCall<UserMiniDetailsType>({
         url: "/auth/login",
         method: "POST",
-        data: data
-    })
-}
+        data: data,
+    });
+};
--- ui/src/containers/Auth/LoginPage.tsx
+++ ui/src/containers/Auth/LoginPage.tsx
@@ -1,47 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {amethyst} from "../../utils/ShadesUtils";
-import {Header4, ResText14Regular, ResText16Regular, ResText16SemiBold} from "../../utils/TextUtils";
+import { amethyst } from "../../utils/ShadesUtils";
+import {
+    Header4,
+    ResText14Regular,
+    ResText16Regular,
+    ResText16SemiBold,
+} from "../../utils/TextUtils";
 import MyButton from "../../components/Button/MyButton";
-import {Form, Input, Modal} from "antd";
-import {capitalize} from "../../utils/StringUtils";
-import {authenticate, login, signup} from "../../api/AuthApi";
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router";
-import {AlertType, openNotification} from "../../utils/Alert";
-import {setAuth} from "../../redux/auth/actions";
-import {selectAuth} from "../../redux/auth/reducer";
+import { Form, Input, Modal } from "antd";
+import { capitalize } from "../../utils/StringUtils";
+import { authenticate, login, signup } from "../../api/AuthApi";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { AlertType, openNotification } from "../../utils/Alert";
+import { setAuth } from "../../redux/auth/actions";
+import { selectAuth } from "../../redux/auth/reducer";
 
 const Wrapper = styled.div`
-  background: ${amethyst};
-  width: 100vw;
-  height: calc(100vh - 48px);
-  overflow: hidden;
+    background: ${amethyst};
+    width: 100vw;
+    height: calc(100vh - 48px);
+    overflow: hidden;
 `;
 
 const LandingCard = styled.div`
-  //background: white;
-  padding: 24px;
-  max-width: 600px;
-  max-height: 500px;
-  margin: auto;
-  border: 1px solid ${amethyst};
-  border-radius: 4px;
-  row-gap: 12px;
+    //background: white;
+    padding: 24px;
+    max-width: 600px;
+    max-height: 500px;
+    margin: auto;
+    border: 1px solid ${amethyst};
+    border-radius: 4px;
+    row-gap: 12px;
 
-  color: white;
+    color: white;
 `;
 
 export default function LoginPage() {
-    const [modals, setModalOpen] = useState({"login": false, "signup": false})
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const {authenticated} = useSelector(selectAuth)
+    const [modals, setModalOpen] = useState({ login: false, signup: false });
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { authenticated } = useSelector(selectAuth);
     const handleModal = (item, action) => {
-        setModalOpen({...modals, [item]: action})
-    }
+        setModalOpen({ ...modals, [item]: action });
+    };
 
-    const handleProfile = (res) => {
+    const handleProfile = res => {
         // const roles = res["roles"]
         // const userMiniDetails: UserMiniDetailsType = {
         //     username: res["username"],
@@ -49,45 +54,55 @@ export default function LoginPage() {
         //     roles: Object.values(UserRoles).filter(item => roles.includes(item))
         // }
         // @ts-ignore
-        dispatch(setAuth(res))
+        dispatch(setAuth(res));
         return navigate("/schedules");
-    }
+    };
 
-    const handleErr = (item, err) => openNotification("Error on " + capitalize(item), err, AlertType.ERROR);
+    const handleErr = (item, err) =>
+        openNotification("Error on " + capitalize(item), err, AlertType.ERROR);
 
     // successful auth: todo:
     // tutor, student => schedules page by default
     // moderator => chat
     const handleSubmit = (item, formInput) => {
         if (formInput["email"] && !formInput["email"].includes("@lsu.edu")) {
-            return openNotification("Invalid email",
+            return openNotification(
+                "Invalid email",
                 "Make sure your email is associated with LSU account.",
-                AlertType.WARNING)
+                AlertType.WARNING,
+            );
         }
         switch (item) {
             case "login":
-                login(formInput).then(res => {
-                    authenticate()
-                        .then(res1 => handleProfile(res1))
-                        .catch(err => handleErr("login", err));
-                }).catch(err => {
-                    openNotification("Error on " + capitalize(item), err, AlertType.ERROR);
-                })
+                login(formInput)
+                    .then(res => {
+                        authenticate()
+                            .then(res1 => handleProfile(res1))
+                            .catch(err => handleErr("login", err));
+                    })
+                    .catch(err => {
+                        openNotification(
+                            "Error on " + capitalize(item),
+                            err,
+                            AlertType.ERROR,
+                        );
+                    });
                 break;
             case "signup":
-                signup(formInput).then(res => {
-                    authenticate()
-                        .then(res1 => handleProfile(res1))
-                        .catch(err => handleErr("sign up", err));
-                }).catch(err => handleErr("signup", err))
+                signup(formInput)
+                    .then(res => {
+                        authenticate()
+                            .then(res1 => handleProfile(res1))
+                            .catch(err => handleErr("sign up", err));
+                    })
+                    .catch(err => handleErr("signup", err));
                 break;
             default:
-                alert("Invalid actions.")
+                alert("Invalid actions.");
         }
-    }
+    };
 
-    if (authenticated)
-        navigate("/schedules");
+    if (authenticated) navigate("/schedules");
 
     return (
         <Wrapper>
@@ -99,59 +114,105 @@ export default function LoginPage() {
                     <u>Find</u> Tutors of your <u>choice</u>
                 </ResText16Regular>
 
-                <MyButton style={{marginTop: 32}} type={"secondary"} onClick={() => handleModal("login", true)}>
+                <MyButton
+                    style={{ marginTop: 32 }}
+                    type={"secondary"}
+                    onClick={() => handleModal("login", true)}
+                >
                     <ResText16Regular>Login</ResText16Regular>
                 </MyButton>
-                <div onClick={() => handleModal("signup", true)} style={{cursor: "pointer"}}>
-                    <ResText16Regular className={"text-underlined"}>New user? Sign up now</ResText16Regular>
+                <div
+                    onClick={() => handleModal("signup", true)}
+                    style={{ cursor: "pointer" }}
+                >
+                    <ResText16Regular className={"text-underlined"}>
+                        New user? Sign up now
+                    </ResText16Regular>
                 </div>
             </LandingCard>
-            {["login", "signup"].map(item => <Modal
-                key={"key-authenticate-" + item}
-                title={<ResText16SemiBold>{capitalize(item)}</ResText16SemiBold>}
-                open={modals[item]}
-                footer={false}
-                onCancel={() => handleModal(item, false)}
-            >
-                <Form name="basic"
-                      layout={"vertical"}
-                      labelCol={{span: 24}}
-                      wrapperCol={{span: 24}}
-                      style={{maxWidth: 600}}
-                      className={"large-vertical-margin"}
-                      onFinish={(values) => handleSubmit(item, values)}
-                      autoComplete="off">
-                    {item === "signup" && <Form.Item
-                        label={<ResText14Regular className={"text-grey2"}>Name</ResText14Regular>}
-                        name="name"
-                        rules={[{required: true, message: 'Enter your name!'}]}
+            {["login", "signup"].map(item => (
+                <Modal
+                    key={"key-authenticate-" + item}
+                    title={
+                        <ResText16SemiBold>
+                            {capitalize(item)}
+                        </ResText16SemiBold>
+                    }
+                    open={modals[item]}
+                    footer={false}
+                    onCancel={() => handleModal(item, false)}
+                >
+                    <Form
+                        name="basic"
+                        layout={"vertical"}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}
+                        style={{ maxWidth: 600 }}
+                        className={"large-vertical-margin"}
+                        onFinish={values => handleSubmit(item, values)}
+                        autoComplete="off"
                     >
-                        <Input/>
-                    </Form.Item>}
-                    <Form.Item
-                        label={<ResText14Regular className={"text-grey2"}>Email</ResText14Regular>}
-                        name="email"
-                        rules={[{required: true, message: 'Enter your email!'}]}
-                    >
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item
-                        label={<ResText14Regular className={"text-grey2"}>Password</ResText14Regular>}
-                        name="password"
-                        rules={[{required: true, message: 'Enter your password!'}]}
-                    >
-                        <Input.Password/>
-                    </Form.Item>
+                        {item === "signup" && (
+                            <Form.Item
+                                label={
+                                    <ResText14Regular className={"text-grey2"}>
+                                        Name
+                                    </ResText14Regular>
+                                }
+                                name="name"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Enter your name!",
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                        )}
+                        <Form.Item
+                            label={
+                                <ResText14Regular className={"text-grey2"}>
+                                    Email
+                                </ResText14Regular>
+                            }
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Enter your email!",
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label={
+                                <ResText14Regular className={"text-grey2"}>
+                                    Password
+                                </ResText14Regular>
+                            }
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Enter your password!",
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
 
-                    <Form.Item>
-                        <div className={"h-end-flex full-width"}>
-                            <MyButton type="primary" htmlType="submit">
-                                {capitalize(item)}
-                            </MyButton>
-                        </div>
-                    </Form.Item>
-                </Form>
-            </Modal>)}
+                        <Form.Item>
+                            <div className={"h-end-flex full-width"}>
+                                <MyButton type="primary" htmlType="submit">
+                                    {capitalize(item)}
+                                </MyButton>
+                            </div>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            ))}
         </Wrapper>
     );
 }
